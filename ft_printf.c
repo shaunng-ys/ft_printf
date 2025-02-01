32114f437edef07f1696fc96bed1ef94158a8f43
Shaun Ng <shaunng_ys@proton.me>
2025-02-02 01:07:11 +0800

    Modified some files

--- ft_printf.c
+++ ft_printf.c
@@ -21,31 +21,41 @@ int	main(void)
 int	ft_printf(const char *string, ...)
 {
 	char		*string_copy;
+	char		char_placeholder;
 	int		nbr_of_inputs;
 	size_t		i;
+	size_t		j;
 	va_list		arg_list;
+	char		*string_placeholder;
 
-	string_copy = string;
 	nbr_of_inputs = 0;
 	i = 0;
-	va_start(arg_list, string_copy);
-	while (string_copy[i])
+	j = 0;
+	va_start(arg_list, string);
+	while (string[i])
 	{
-    		if (string_copy[i] != '%')
-			ft_putchar(string_copy[i++], 1);
-		else if (string_copy[i] == '%')
+    		if (string[i] != '%')
+			ft_putchar_fd(string[i++], 1);
+		else if (string[i] == '%')
 		{
 			nbr_of_inputs++;
 			i++;
-			if (string_copy[i] == c)
-				ft_putchar(va_arg(arg_list, char), 1);
-			else if (string_copy[i] == s)
-				while (va_arg(arg_list, char *))[j], 1)
-					ft_putchar(va_arg(arg_list, char *)[j++], 1);
-			i++;
+			if (string[i] == 'c')
+			{
+				char_placeholder = va_arg(arg_list, int);
+				ft_putchar_fd(char_placeholder, 1);
+				i++;
+			}
+			else if (string[i] == 's')
+			{
+				string_placeholder = va_arg(arg_list, char *);
+				while (string_placeholder[j])
+					ft_putchar_fd(string_placeholder[j++], 1);
+				i++;
+				j = 0;
+			}
 		}
 	}
-}
 	// then look in the va_list for the next argument and run the function
 	// that will print that argument
 	return (nbr_of_inputs);
